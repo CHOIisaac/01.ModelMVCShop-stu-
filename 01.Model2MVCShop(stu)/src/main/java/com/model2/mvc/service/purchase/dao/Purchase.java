61205105198d35b5c04c36be23d1943ded04c843
bitcamp <bitcamp@DESKTOP-R07AUTB>
2022-04-29 19:46:08 +0900

    2022.04.29 first commit

---
+++ 01.Model2MVCShop(stu)/src/main/java/com/model2/mvc/service/purchase/dao/Purchase.java
@@ -0,0 +1,25 @@
+package com.model2.mvc.service.purchase.dao;
+
+import java.util.HashMap;
+
+import com.model2.mvc.common.SearchVO;
+import com.model2.mvc.service.purchase.vo.PurchaseVO;
+
+public class Purchase {
+	
+	public Purchase() {
+	}
+	
+	public void insertPurchase(PurchaseVO purchaseVO) throws Exception {
+		
+	}
+	
+	public Purchase findPurchase() throws Exception {
+		return null;
+		
+	}
+	
+	public HashMap<String, Object> getPurchaseList(SearchVO searchVO, String string){
+		return null;
+	}
+}
